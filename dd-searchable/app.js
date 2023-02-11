eab41238f6f9c4c6bd8be3a82e39fb6a46680750
ankkumar41 <ankkumar41@publicisgroupe.net>
2023-02-11 19:04:17 +0530

    s103

--- dd-searchable/app.js
+++ dd-searchable/app.js
@@ -5,27 +5,30 @@ const MAP_NAMESPACE = {
   STR_SELECTED: "selected",
   STR_TRUE: "1",
   STR_FALSE: "0",
+  STR_HIDDEN: "hidden",
+  STR_VISIBLE: "visible",
   BOOL_TRUE: true,
   BOOL_FALSE: false,
   CLASS_SEARCHABLE_WRAPPER: ".dd-searchable-wrapper",
-  CLASS_SEARCHABLE_MENU: ".dd-searchable-menu",
-  CLASS_SELECT_FIELD: ".select-field-container",
-  CLASS_INPUT: ".select-field-value",
+  CLASS_SEARCHABLE_MENU: ".dd-searchable-wrapper .dd-searchable-menu",
+  CLASS_SELECT_FIELD: ".dd-searchable-wrapper .select-field-container",
+  CLASS_INPUT: ".dd-searchable-wrapper .select-field-value",
   CLASS_MENU_LIST: ".dd-searchable-wrapper .menu-list-container",
   CLASS_MENU_LIST_ITEM: ".dd-searchable-wrapper .menu-list-container .menu-list-item",
   CLASS_ERROR_STATUS: ".dd-searchable-wrapper .error-status",
-  CLASS_LOCATION_ITEM: ".location-item",
-  CLASS_REGION: ".region",
-  CLASS_HIDDEN: "hidden",
-  CLASS_VISIBLE: "visible",
+  CLASS_LOCATION_ITEM: ".dd-searchable-wrapper .location-item",
+  CLASS_REGION: ".dd-searchable-wrapper .region",
+  EVENT_CLICK: "click",
+  EVENT_INPUT: "input",
 };
 
-const $ = (domNode, query) => {
-  const els = domNode.querySelectorAll(query);
-  if (els.length > 1) {
-    return els;
-  } else if (els.length === 1) {
-    return els[0];
+const $ = (node, query) => {
+  const nodeList = node.querySelectorAll(query);
+
+  if (nodeList.length > 1) {
+    return nodeList;
+  } else if (nodeList.length === 1) {
+    return nodeList[0];
   } else {
     return null;
   }
@@ -45,13 +48,34 @@ const store = {
       menuListContainer: $(document, MAP_NAMESPACE.CLASS_MENU_LIST),
       errorStatusNode: $(document, MAP_NAMESPACE.CLASS_ERROR_STATUS),
     },
-    setState: (updateStore, render) => {
+    setState: (updateStore, render, handleSideEffect) => {
       if (updateStore !== undefined) updateStore();
       if (render !== undefined) render();
-
+      if (handleSideEffect !== undefined) handleSideEffect();
       return;
     },
   },
+};
+
+const shouldRegionBeChecked = node => {
+  return [...node.parentElement.children].every(node => {
+    const isSelected = node.getAttribute(MAP_NAMESPACE.STR_SELECTED);
+    return isSelected === MAP_NAMESPACE.STR_TRUE;
+  });
+};
+
+const applyCheckBox = node => {
+  node[0].classList.remove(MAP_NAMESPACE.STR_VISIBLE);
+  node[0].classList.add(MAP_NAMESPACE.STR_HIDDEN);
+  node[1].classList.remove(MAP_NAMESPACE.STR_HIDDEN);
+  node[1].classList.add(MAP_NAMESPACE.STR_VISIBLE);
+};
+
+const removeCheckBox = node => {
+  node[0].classList.remove(MAP_NAMESPACE.STR_HIDDEN);
+  node[0].classList.add(MAP_NAMESPACE.STR_VISIBLE);
+  node[1].classList.remove(MAP_NAMESPACE.STR_VISIBLE);
+  node[1].classList.add(MAP_NAMESPACE.STR_HIDDEN);
 };
 
 const debounce = (callback, delay) => {
@@ -64,7 +88,7 @@ const debounce = (callback, delay) => {
 
 const populateOptions = () => {
   store.searchable.state.menuListContainer.innerHTML = `
-    ${[...store.searchable.state.orignalListOfLocations].map(domNode => domNode.outerHTML).join("")}
+    ${[...store.searchable.state.orignalListOfLocations].map(node => node.outerHTML).join("")}
     `;
 
   return;
@@ -140,77 +164,9 @@ const filterDropMenu = searchableMenuNode => {
   renderFilterResultInDOM(filteredArray, searchableMenuNode);
 };
 
-window.onload = () => {
-  const inputNode = $(store.searchable.state.searchableWrapperNode, MAP_NAMESPACE.CLASS_INPUT);
-  const searchableMenuNode = $(store.searchable.state.searchableWrapperNode, MAP_NAMESPACE.CLASS_SEARCHABLE_MENU);
-
-  document.body.addEventListener("click", () => {
-    store.searchable.setState(
-      () => {
-        store.searchable.state.isDropdownActive = MAP_NAMESPACE.BOOL_FALSE;
-        store.searchable.state.searchedKeyword = "";
-      },
-      () => {
-        searchableMenuNode.classList.remove(MAP_NAMESPACE.STR_ACTIVE);
-
-        const selectionListArraySize = store.searchable.state.selectionList.length;
-
-        if (selectionListArraySize === 0) {
-          inputNode.setAttribute(MAP_NAMESPACE.STR_VALUE, "");
-          inputNode.value = "";
-        } else if (selectionListArraySize === 1) {
-          inputNode.setAttribute(MAP_NAMESPACE.STR_VALUE, `${store.searchable.state.selectionList[0]}`);
-          inputNode.value = `${store.searchable.state.selectionList[0]}`;
-        } else {
-          inputNode.setAttribute(
-            MAP_NAMESPACE.STR_VALUE,
-            `${store.searchable.state.selectionList[0]} +${store.searchable.state.selectionList.length - 1}`
-          );
-          inputNode.value = `${store.searchable.state.selectionList[0]} +${
-            store.searchable.state.selectionList.length - 1
-          }`;
-        }
-      }
-    );
-  });
-
-  $(store.searchable.state.searchableWrapperNode, MAP_NAMESPACE.CLASS_SELECT_FIELD).addEventListener("click", event => {
-    event.stopPropagation();
-
-    store.searchable.setState(
-      () => {
-        store.searchable.state.isDropdownActive = MAP_NAMESPACE.BOOL_TRUE;
-        store.searchable.state.searchedKeyword = "";
-      },
-      () => {
-        searchableMenuNode.classList.add(MAP_NAMESPACE.STR_ACTIVE);
-        inputNode.focus();
-
-        inputNode.setAttribute(MAP_NAMESPACE.STR_VALUE, "");
-        inputNode.value = "";
-        populateOptions();
-      }
-    );
-  });
-
-  inputNode.addEventListener("input", event => {
-    debounce(() => {
-      store.searchable.setState(
-        () => {
-          store.searchable.state.searchedKeyword = event.target.value;
-        },
-        () => {
-          inputNode.setAttribute(MAP_NAMESPACE.STR_VALUE, event.target.value);
-
-          filterDropMenu(searchableMenuNode);
-        }
-      );
-    }, store.delay);
-  });
-};
-
 const regionSelectionHandler = (event, node) => {
   event.stopPropagation();
+
   const getter = node.getAttribute(MAP_NAMESPACE.STR_SELECTED);
 
   store.searchable.setState(
@@ -224,10 +180,9 @@ const regionSelectionHandler = (event, node) => {
             .trim();
 
           array.push(location);
-
-          const unique = [...new Set(array)];
-          store.searchable.state.selectionList = unique;
         }
+
+        store.searchable.state.selectionList = [...new Set([...store.searchable.state.selectionList, ...array])];
       } else {
         for (let i = 0; i < [...node.nextElementSibling.children].length; i++) {
           const location = node.nextElementSibling.children[i].lastElementChild
@@ -241,33 +196,20 @@ const regionSelectionHandler = (event, node) => {
         }
       }
     },
+
     () => {
       if (getter === MAP_NAMESPACE.STR_FALSE) {
-        node.children[0].classList.remove(MAP_NAMESPACE.CLASS_VISIBLE);
-        node.children[0].classList.add(MAP_NAMESPACE.CLASS_HIDDEN);
-        node.children[1].classList.remove(MAP_NAMESPACE.CLASS_HIDDEN);
-        node.children[1].classList.add(MAP_NAMESPACE.CLASS_VISIBLE);
+        applyCheckBox(node.children);
 
         for (let i = 0; i < node.nextElementSibling.children.length; i++) {
-          node.nextElementSibling.children[i].children[0].classList.remove(MAP_NAMESPACE.CLASS_VISIBLE);
-          node.nextElementSibling.children[i].children[0].classList.add(MAP_NAMESPACE.CLASS_HIDDEN);
-          node.nextElementSibling.children[i].children[1].classList.remove(MAP_NAMESPACE.CLASS_HIDDEN);
-          node.nextElementSibling.children[i].children[1].classList.add(MAP_NAMESPACE.CLASS_VISIBLE);
-
+          applyCheckBox(node.nextElementSibling.children[i].children);
           node.nextElementSibling.children[i].setAttribute(MAP_NAMESPACE.STR_SELECTED, MAP_NAMESPACE.STR_TRUE);
         }
       } else {
-        node.children[0].classList.remove(MAP_NAMESPACE.CLASS_HIDDEN);
-        node.children[0].classList.add(MAP_NAMESPACE.CLASS_VISIBLE);
-        node.children[1].classList.remove(MAP_NAMESPACE.CLASS_VISIBLE);
-        node.children[1].classList.add(MAP_NAMESPACE.CLASS_HIDDEN);
+        removeCheckBox(node.children);
 
         for (let i = 0; i < node.nextElementSibling.children.length; i++) {
-          node.nextElementSibling.children[i].children[0].classList.remove(MAP_NAMESPACE.CLASS_HIDDEN);
-          node.nextElementSibling.children[i].children[0].classList.add(MAP_NAMESPACE.CLASS_VISIBLE);
-          node.nextElementSibling.children[i].children[1].classList.remove(MAP_NAMESPACE.CLASS_VISIBLE);
-          node.nextElementSibling.children[i].children[1].classList.add(MAP_NAMESPACE.CLASS_HIDDEN);
-
+          removeCheckBox(node.nextElementSibling.children[i].children);
           node.nextElementSibling.children[i].setAttribute(MAP_NAMESPACE.STR_SELECTED, MAP_NAMESPACE.STR_FALSE);
         }
       }
@@ -281,6 +223,15 @@ const regionSelectionHandler = (event, node) => {
         MAP_NAMESPACE.STR_VALUE,
         store.searchable.state.selectionList.join(";")
       );
+    },
+    () => {
+      store.searchable.state.orignalListOfLocations = [...store.searchable.state.orignalListOfLocations].map(item => {
+        if (item.children[0].id === node.id) {
+          return node.parentElement;
+        } else {
+          return item;
+        }
+      });
     }
   );
 };
@@ -303,15 +254,9 @@ const LocationSelectionHandler = (event, node) => {
 
     () => {
       if (getter === MAP_NAMESPACE.STR_FALSE) {
-        node.children[0].classList.remove(MAP_NAMESPACE.CLASS_VISIBLE);
-        node.children[0].classList.add(MAP_NAMESPACE.CLASS_HIDDEN);
-        node.children[1].classList.remove(MAP_NAMESPACE.CLASS_HIDDEN);
-        node.children[1].classList.add(MAP_NAMESPACE.CLASS_VISIBLE);
+        applyCheckBox(node.children);
       } else {
-        node.children[0].classList.remove(MAP_NAMESPACE.CLASS_HIDDEN);
-        node.children[0].classList.add(MAP_NAMESPACE.CLASS_VISIBLE);
-        node.children[1].classList.remove(MAP_NAMESPACE.CLASS_VISIBLE);
-        node.children[1].classList.add(MAP_NAMESPACE.CLASS_HIDDEN);
+        removeCheckBox(node.children);
       }
 
       node.setAttribute(
@@ -321,16 +266,10 @@ const LocationSelectionHandler = (event, node) => {
 
       if (shouldRegionBeChecked(node)) {
         node.parentElement.previousElementSibling.setAttribute(MAP_NAMESPACE.STR_SELECTED, MAP_NAMESPACE.STR_TRUE);
-        node.parentElement.previousElementSibling.children[0].classList.remove(MAP_NAMESPACE.CLASS_VISIBLE);
-        node.parentElement.previousElementSibling.children[0].classList.add(MAP_NAMESPACE.CLASS_HIDDEN);
-        node.parentElement.previousElementSibling.children[1].classList.remove(MAP_NAMESPACE.CLASS_HIDDEN);
-        node.parentElement.previousElementSibling.children[1].classList.add(MAP_NAMESPACE.CLASS_VISIBLE);
+        applyCheckBox(node.parentElement.previousElementSibling.children);
       } else {
         node.parentElement.previousElementSibling.setAttribute(MAP_NAMESPACE.STR_SELECTED, MAP_NAMESPACE.STR_FALSE);
-        node.parentElement.previousElementSibling.children[0].classList.remove(MAP_NAMESPACE.CLASS_HIDDEN);
-        node.parentElement.previousElementSibling.children[0].classList.add(MAP_NAMESPACE.CLASS_VISIBLE);
-        node.parentElement.previousElementSibling.children[1].classList.remove(MAP_NAMESPACE.CLASS_VISIBLE);
-        node.parentElement.previousElementSibling.children[1].classList.add(MAP_NAMESPACE.CLASS_HIDDEN);
+        removeCheckBox(node.parentElement.previousElementSibling.children);
       }
 
       store.searchable.state.searchableWrapperNode.setAttribute(
@@ -341,9 +280,79 @@ const LocationSelectionHandler = (event, node) => {
   );
 };
 
-const shouldRegionBeChecked = node => {
-  return [...node.parentElement.children].every(node => {
-    const isSelected = node.getAttribute(MAP_NAMESPACE.STR_SELECTED);
-    return isSelected === MAP_NAMESPACE.STR_TRUE;
+window.onload = () => {
+  const inputNode = $(store.searchable.state.searchableWrapperNode, MAP_NAMESPACE.CLASS_INPUT);
+  const searchableMenuNode = $(store.searchable.state.searchableWrapperNode, MAP_NAMESPACE.CLASS_SEARCHABLE_MENU);
+
+  document.body.addEventListener(MAP_NAMESPACE.EVENT_CLICK, () => {
+    store.searchable.setState(
+      () => {
+        store.searchable.state.isDropdownActive = MAP_NAMESPACE.BOOL_FALSE;
+        store.searchable.state.searchedKeyword = "";
+      },
+      () => {
+        searchableMenuNode.classList.remove(MAP_NAMESPACE.STR_ACTIVE);
+        store.searchable.state.errorStatusNode.classList.remove(MAP_NAMESPACE.STR_ACTIVE);
+
+        const selectionListArraySize = store.searchable.state.selectionList.length;
+
+        if (selectionListArraySize === 0) {
+          inputNode.setAttribute(MAP_NAMESPACE.STR_VALUE, "");
+          inputNode.value = "";
+        } else if (selectionListArraySize === 1) {
+          inputNode.setAttribute(MAP_NAMESPACE.STR_VALUE, `${store.searchable.state.selectionList[0]}`);
+          inputNode.value = `${store.searchable.state.selectionList[0]}`;
+        } else {
+          inputNode.setAttribute(
+            MAP_NAMESPACE.STR_VALUE,
+            `${store.searchable.state.selectionList[0]} +${store.searchable.state.selectionList.length - 1}`
+          );
+          inputNode.value = `${store.searchable.state.selectionList[0]} +${
+            store.searchable.state.selectionList.length - 1
+          }`;
+        }
+      }
+    );
+  });
+
+  $(store.searchable.state.searchableWrapperNode, MAP_NAMESPACE.CLASS_SELECT_FIELD).addEventListener(
+    MAP_NAMESPACE.EVENT_CLICK,
+    event => {
+      event.stopPropagation();
+
+      store.searchable.setState(
+        () => {
+          store.searchable.state.isDropdownActive = MAP_NAMESPACE.BOOL_TRUE;
+          store.searchable.state.searchedKeyword = "";
+        },
+        () => {
+          searchableMenuNode.classList.add(MAP_NAMESPACE.STR_ACTIVE);
+          inputNode.focus();
+
+          inputNode.setAttribute(MAP_NAMESPACE.STR_VALUE, "");
+          inputNode.value = "";
+
+          if (store.searchable.state.selectionList.length === 0) {
+            populateOptions();
+          }
+        }
+      );
+    }
+  );
+
+  inputNode.addEventListener(MAP_NAMESPACE.EVENT_INPUT, event => {
+    debounce(() => {
+      store.searchable.setState(
+        () => {
+          store.searchable.state.searchedKeyword = event.target.value;
+        },
+        () => {
+          inputNode.setAttribute(MAP_NAMESPACE.STR_VALUE, event.target.value);
+          inputNode.value = event.target.value;
+
+          filterDropMenu(searchableMenuNode);
+        }
+      );
+    }, store.delay);
   });
 };
